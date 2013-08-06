caec5a7ddeb5f4bd0a3eb44cebd1cb851575b1e3
unknown <SEnnis@SENNIS764.navimedix.com>
2013-08-06 09:59:50 -0400

    second commit

---
+++ NewsLibrarySearchWebUI/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="NewsLibrarySearchUI._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <p>You can use the operators AND, OR, and NOT to refine results, as well as quotation marks.</p>
+
+    <p>Enter terms to search with or without boolean operators</p>
+    <asp:TextBox ID="SearchForm" CssClass="SearchForm" runat="server">Search Terms</asp:TextBox>
+
+    <asp:DropDownList ID="FieldTargetsList" runat="server">
+        <asp:ListItem>All Text</asp:ListItem>
+        <asp:ListItem>Lede</asp:ListItem>
+        <asp:ListItem>Headline</asp:ListItem>
+        <asp:ListItem>Index Terms</asp:ListItem>
+        <asp:ListItem>Author</asp:ListItem>
+        <asp:ListItem>Section</asp:ListItem>
+        <asp:ListItem>Edition</asp:ListItem>
+        <asp:ListItem>Caption</asp:ListItem>
+        <asp:ListItem>Page</asp:ListItem>
+        <asp:ListItem>Source</asp:ListItem>
+    </asp:DropDownList>
+    
+
+    <p>Enter a date range to search or choose from the dropdown menu. Defaults to all documents. </p>
+    <asp:TextBox ID="DateRangeFrom" CssClass="field" runat="server">Start date</asp:TextBox>  &nbsp; to &nbsp; 
+    <asp:TextBox ID="DateRangeTo" CssClass="field" runat="server">End date</asp:TextBox>
+    
+    <p>Add search terms and click update to view results</p>
+    <asp:Button ID="newSearch" runat="server" Text="Add search" OnClick="AddSearchMethod"/>
+    <asp:Button ID="addTerm" runat="server" Text="Clear search" OnClick="ClearSearchMethod"/>
+    
+    <asp:ScriptManager ID="ScriptManager" runat="server" />
+    <asp:UpdatePanel ID="UpdatePanel1" CssClass="updatePanel" UpdateMode="Always" Visible="False" runat="server">
+        <ContentTemplate>
+            <fieldset>
+                <asp:Label ID="resultsLabel" runat="server" Text=""></asp:Label>
+                <asp:Timer ID="Timer1" Interval="1000" OnTick="UpdateMethod" runat="server">
+                </asp:Timer>
+                <asp:GridView CssClass="dataDisplay" ID="resultsGV" runat="server" 
+                    AllowSorting="true" AutoGenerateColumns="false" OnSorting="OnSort">
+                    <Columns>
+                        <asp:BoundField HeaderText="Term" DataField="SearchTerm" SortExpression="SearchTerm" ItemStyle-HorizontalAlign="Left" HeaderStyle-HorizontalAlign="Left"/>
+                        <asp:BoundField HeaderText="Date" DataField="Date" SortExpression="Date" ItemStyle-HorizontalAlign="Left" HeaderStyle-HorizontalAlign="Left"/>
+                        <asp:BoundField HeaderText="Field" DataField="FieldTarget" SortExpression="FieldTarget" ItemStyle-HorizontalAlign="Left" HeaderStyle-HorizontalAlign="Left"/>
+                        <asp:BoundField HeaderText="Occurances" DataField="Count" SortExpression="Count" ItemStyle-HorizontalAlign="Left" HeaderStyle-HorizontalAlign="Left"/>
+                    </Columns>
+                
+                </asp:GridView>
+            </fieldset>
+        </ContentTemplate>
+        
+    </asp:UpdatePanel>
+    
+</asp:Content>
+
+<asp:Content ID="rightColumnContent" runat="server" ContentPlaceHolderID="SidebarContent">
+    
+</asp:Content>
---
+++ NewsLibrarySearchWebUI/Default.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
+using NewsLibrarySearch;
+
+
+namespace NewsLibrarySearchUI
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        private NlAdvancedSearch _search;
+        private static int _counter = 0;
+        public SortedList<long, NlSearchResult> resultsList;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get list for given session
+            resultsList = HttpContext.Current.Session["List"] as SortedList<long, NlSearchResult>;
+            if (null == resultsList)
+            {
+                resultsList = new SortedList<long, NlSearchResult>();
+                HttpContext.Current.Session["List"] = resultsList;
+            }
+
+            //Set gridView appearance characteristics
+            resultsGV.DataSource = resultsList.Values;
+            resultsGV.GridLines = GridLines.None;
+            resultsGV.HeaderStyle.HorizontalAlign = HorizontalAlign.Left;
+            resultsGV.HorizontalAlign = HorizontalAlign.Left;
+            resultsGV.Width = 720;
+            resultsGV.CellPadding = 4;
+            resultsGV.BorderWidth = 0;
+        }
+
+        protected void AddSearchMethod(object sender, EventArgs e)
+        {
+            // gets dates, 
+            var date = "";
+
+            // concatonate dates if they are not blank
+            if (DateRangeFrom.Text != "" && DateRangeTo.Text != "")
+            {
+                DateTime dateFrom = DateTime.Parse(DateRangeFrom.Text);
+                DateTime dateTo = DateTime.Parse(DateRangeTo.Text);
+                date = dateFrom.ToString("MMMM dd, yyyy") + " to " + dateTo.ToString("MMMM dd, yyyy");
+            }
+        
+            //Get searchTerm and fieldTarget
+            var searchTerm = SearchForm.Text;
+            var fieldTarget = NlSearchTargets.Target(FieldTargetsList.Text);
+
+            //Create new task
+            long now = DateTime.Now.Ticks;
+            Task.Factory.StartNew(() => MakeRequest(date, searchTerm, fieldTarget, now));
+            UpdatePanel1.Visible = true;
+        }
+
+        protected void UpdateMethod(object sender, EventArgs e)
+        {
+            //Update label with contents of resultsList
+      //      resultsLabel.Text = "<table width=760px>";
+      //      foreach (var v in resultsList.Values)
+      //      {
+      //          string fieldKey = NlSearchTargets.Key(v.FieldTarget);
+      //          resultsLabel.Text += "<tr><td class=\"resultsTable\" width=240px>" + v.SearchTerm + "</td><td>" +v.Date + "</td><td>   in " + fieldKey.ToLower() + "</td><td>  " + v.Count + " occurances</td></tr>";
+      //      }
+      //      resultsLabel.Text += "</table>";
+
+            
+            resultsGV.DataBind();
+            //UpdatePanel1.Update();
+        }
+
+        protected void ClearSearchMethod(object sender, EventArgs e)
+        {
+            //Clear resultsLabel and clear resultsList
+            UpdatePanel1.Visible = false;
+            resultsLabel.Text = "";
+            resultsList.Clear();
+        }
+
+     //   protected void OnSort(object sender, EventArgs e)
+     //   {
+     //       resultsGV.Sort("Count", SortDirection.Descending);
+     //   }
+
+
+        protected void OnSort(object sender, GridViewSortEventArgs e)
+        {
+            DataTable dataTable = resultsList.Values as DataTable;
+            if (dataTable != null)
+            {
+                DataView dataView = new DataView(dataTable);
+                dataView.Sort= e.SortExpression +" " + e.SortDirection;
+
+                resultsGV.DataSource = dataView;
+                resultsGV.DataBind();
+            }
+
+
+        }
+
+        protected void MakeRequest(string date, string searchTerm, string fieldTarget, long now)
+        {
+            //Create a new search object
+            _search = new NlAdvancedSearch();
+
+            //Increment counter for ordered placement. redundant with increment below?
+            _counter++;
+
+            //Create search result to populate label
+            NlSearchResult s = new NlSearchResult
+            {
+                SearchTerm = searchTerm,
+                Date = date,
+                FieldTarget = fieldTarget,
+                Count = _search.SendSearchRequest(date, searchTerm, fieldTarget)
+            };
+
+            resultsList.Add(now, s);
+        }
+
+
+    }
+}

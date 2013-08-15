5607613ff75e76d1b67e3a0d75892433f868af82
unknown <SEnnis@SENNIS764.navimedix.com>
2013-08-15 16:20:43 -0400

    Client update 0.0.2

--- NewsLibrarySearchWebUI/NewsLibrary.aspx
+++ NewsLibrarySearchWebUI/NewsLibrary.aspx
@@ -7,9 +7,15 @@
     <script src="Scripts/jquery-1.4.1.min.js" type="text/javascript"></script>
      <script src="Scripts/NlSearchScripts.js" type="text/javascript" ></script>
 
+     <p>You can use the operators AND, OR, and NOT to refine results, as well as quotation marks.</p>
+     <p>Enter terms to search with or without boolean operators</p>
+     
+     <input type="text" id="searchText" class="SearchForm" placeholder="Enter search terms"/>
     
     
+    <input type = "button" id="newSearchButton" onclick="searchClick()" value="New search"/>
+    <input type = "button" id="clearSearchButton" value="Clear search"/>
     
-    
+    <div id="crap"></div>
 
 </asp:Content>
--- NlSearchLib/NlSearchBody.cs
+++ NlSearchLib/NlSearchBody.cs
@@ -30,7 +30,7 @@
 
             _searchTerm = searchTerm;
             _fieldTarget = fieldTarget;
-            _fieldTarget = "";
+            //_fieldTarget = "";
             _bodyHeader = @"s_siteloc=NL2&p_queryname=4000&p_action=search&p_product=NewsLibrary&p_theme=newslibrary2&s_search_type=customized&d_sources=location&d_place=United+States&p_nbid=&p_field_psudo-sort-0=psudo-sort&f_multi=&p_multi=";
             _bodyMiddle = bodyMiddleDefault();
             _bodyFooter = "&p_widesearch=smart&p_sort=YMD_date%3AD&p_maxdocs=200&p_perpage=10&p_text_base-0=" + _searchTerm + FieldBase + fieldTarget + "&p_bool_base-1=AND&p_text_base-1=&p_field_base-1=Section&p_bool_base-2=AND&p_text_base-2=" + "&p_field_base-2=" + DateBase + _date + "&p_field_YMD_date-0=YMD_date&p_params_YMD_date-0=date%3AB%2CE&p_field_YMD_date-3=YMD_date&p_params_YMD_date-3=date%3AB%2CE&Search.x=18&Search.y=18";

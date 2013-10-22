724ae831cdd74fab79ca351ce545097cd4672ae7
upquark <sam.e.ennis@gmail.com>
2013-10-22 15:50:18 -0400

    Initial construction of jscript frontend for census API

---
+++ DataNervesUI/Areas/NewsLibrary/NewsLibraryAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DataNervesUI.Areas.NewsLibrary
+{
+    public class NewsLibraryAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "NewsLibrary";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "NewsLibrary_default",
+                "NewsLibrary/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
